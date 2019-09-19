e6ed31dbf5ef7ba48af04eb8b41aa1cece01d951
Benjamin Lee <benjamindlee4@gmail.com>
2019-09-19 16:35:20 -0500

    added more js, made data apper on page, your guesses show entire alphabet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,8 @@
+
 var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
                 "p","q","r","s","t","u","v","w","x","y","z"];
 var wins = 0;
-var loses = 0;
+var losses = 0;
 var guessesLeft = 9;
 var yourGuesses = [];
 
@@ -9,7 +10,7 @@ document.onkeydown = function(event) {
 
     var userGuess = event.key;
 
-    var comGuess = letters (Math.floor(Math.random() * letters.length));
+    var comGuess = letters [Math.floor(Math.random() * letters.length)];
 
     var userInput = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
     "p","q","r","s","t","u","v","w","x","y","z"];
@@ -22,21 +23,36 @@ document.onkeydown = function(event) {
         }
 
         if (userGuess != comGuess){
-            guessesLeft = -1;
+            guessesLeft --;
             yourGuesses.push(userInput);
         }
 
         if (guessesLeft === 0){
-            loses++;
+            losses++;
             guessesLeft = 9;
             yourGuesses = [];
         }
     }
 
-    var onPageInfo =
+    var html =
+    "<h1>The Psychic Game</h1>" +
+    "<p>Guess what letter im thinking of</p>" +
+    "<p>Wins: " + wins + "</p>" +
+    "<p>Losses: " + losses + "</p>" +
+    "<p>Guesses Left: " + guessesLeft + "</p>" +
+    "<p>Your guesses so far: " + yourGuesses.join(",") + "</p>" 
+        
+    document.querySelector("#game").innerHTML = html;
+
+
+
+console.log(userGuess);
+console.log(comGuess);
+console.log(wins);
+console.log(losses);
+console.log(guessesLeft);
+
+
+
 
 }
-
-
-
-
